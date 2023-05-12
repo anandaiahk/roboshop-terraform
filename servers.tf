50e6bba089470568268bc2841d0ca1e388832597
anandk <kanandaiah9@gmail.com>
2023-05-12 11:23:03 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -1,26 +1,31 @@
 resource "aws_instance" "instance" {
-  for_each =  var.components
-  ami           =  data.aws_ami.centos.image_id
-  instance_type = each.value["instance_type"]
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-  tags = {
+  for_each               = var.components
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = each.value["instance_type"]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  tags                   = {
     Name = each.value["name"]
   }
-  provisioner "remote-exec" {
+}
+  resource "null_resource" "provisioner" {
+    depends_on = [aws_instance.instance, aws_route53_record_records]
+    for_each               = var.components
+    provisioner "remote-exec" {
 
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
-      host     = self.private_ip
+      connection {
+        type     = "ssh"
+        user     = "centos"
+        password = "DevOps321"
+        host     = aws_instance.instance[each.value["name"]].private_ip
+      }
+      inline = [
+        "rm -rf roboshop-shell",
+
+        "git clone https://https://github.com/anandaiahk/roboshop-shell",
+        "cd roboshop-shell",
+        "sudo bash ${each.value["name"]}",
+      ]
     }
-    inline = [
-      "rm -rf roboshop-shell",
-
-      "git clone https://https://github.com/anandaiahk/roboshop-shell",
-      "cd roboshop-shell",
-      "sudo bash ${each.value["name"]}",
-    ]
   }
 }
 
